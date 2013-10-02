4aa2852f80274df1076221293e42bbe0d6cdd659
Javi <Javi@Javi-PC>
2013-10-02 20:09:06 +0200

    hasta el ejercicio d) correcto, muestra para todos los coches su marca y modelo. Encuentra dni y compara con dni para imprimir modelo del coche. Imprime todos los objetos de marca "Ford" (cilindrada, dniPropietario y modelo).
    Siguiente ejercicio:
    
    Creacion del map
    Añadir objetos al map.
    imprimir el objeto con un determinado dni
    recorre el map y obtiene los objetos.

--- src/colecciones/Colecciones.java
+++ src/colecciones/Colecciones.java
@@ -21,11 +21,11 @@ public class Colecciones {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+
         String entradaTeclado;
-        
+
         Map<String, Coche> mapCoches = new HashMap<>();
-        
+
         List<Coche> coches = new ArrayList<>();
         Coche coche1 = new Coche(Marca.Ford, "Focus", 1000, "48599967C");
         Coche coche2 = new Coche(Marca.Ford, "Mustang", 2500, "12345678Z");
@@ -39,27 +39,33 @@ public class Colecciones {
         coches.add(coche3);
         coches.add(coche4);
         coches.add(coche5);
-        
+
         //añadimos al map los objetos.
-        mapCoches.put(coche1.getDniPropietario(),coche1);
-        mapCoches.put(coche2.getDniPropietario(),coche2);
-        mapCoches.put(coche3.getDniPropietario(),coche3);
-        mapCoches.put(coche4.getDniPropietario(),coche4);
-        mapCoches.put(coche5.getDniPropietario(),coche5);
-        
-        
+        mapCoches.put(coche1.getDniPropietario(), coche1);
+        mapCoches.put(coche2.getDniPropietario(), coche2);
+        mapCoches.put(coche3.getDniPropietario(), coche3);
+        mapCoches.put(coche4.getDniPropietario(), coche4);
+        mapCoches.put(coche5.getDniPropietario(), coche5);
+
+
         Coche i = mapCoches.get("12345678Z");
-        System.out.println(i.getModelo()+" "+i.getMarca()+" "+i.getCilindrada());
-/*
-        System.out.println(coche1.getMarca());
+        System.out.println(i.getModelo() + " " + i.getMarca() + " " + i.getCilindrada());
 
-
-        for (int i = 0; i < coches.size(); i++) {
-            System.out.println(coches.get(i).getMarca()+" "+coches.get(i).getModelo());
+        for (String clave : mapCoches.keySet()) {
+            Coche valor = mapCoches.get(clave);
+            System.out.println(clave + " = " + valor.getMarca() +" "+ valor.getModelo());
         }
+
+        /*
+         System.out.println(coche1.getMarca());
+
+
+         for (int i = 0; i < coches.size(); i++) {
+         System.out.println(coches.get(i).getMarca()+" "+coches.get(i).getModelo());
+         }
         
         
-        System.out.println("Introduzca un dni: ");
+         System.out.println("Introduzca un dni: ");
          Scanner entradaEscaner = new Scanner (System.in);
          
          entradaTeclado = entradaEscaner.nextLine ();
@@ -67,16 +73,16 @@ public class Colecciones {
          
          for (int i = 0; i < coches.size(); i++) {
              
-            if(entradaTeclado.equals(coches.get(i).getDniPropietario()) ){
-                System.out.println(coches.get(i).getModelo());
-            }
-        }
+         if(entradaTeclado.equals(coches.get(i).getDniPropietario()) ){
+         System.out.println(coches.get(i).getModelo());
+         }
+         }
          for (int i = 0; i < coches.size(); i++) {
-             if(coches.get(i).getMarca().equals(Marca.Ford)){
-                 System.out.println(coches.get(i).getModelo()+" "+coches.get(i).getDniPropietario()+" "+coches.get(i).getCilindrada());
-             }
+         if(coches.get(i).getMarca().equals(Marca.Ford)){
+         System.out.println(coches.get(i).getModelo()+" "+coches.get(i).getDniPropietario()+" "+coches.get(i).getCilindrada());
+         }
             
-        }
+         }
          */
 
     }
